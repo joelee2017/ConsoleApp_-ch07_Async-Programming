4c07b5b6d2d7242bbfae9346d3fc4bf33aef3320
joe lee <as955133@gmail.com>
2018-06-25 13:16:28 +0800

    改用 C# 5.0  async 與 await

--- WPF_asyncAndawait關鍵字/MainWindow.xaml.cs
+++ WPF_asyncAndawait關鍵字/MainWindow.xaml.cs
@@ -26,56 +26,70 @@ namespace WPF_asyncAndawait關鍵字
             InitializeComponent();
         }
 
-        //C# 4.0 寫法
-        //private void Start_Click(object sender, RoutedEventArgs e)
-        //{
-        //    AccessUrlForNet4Async();
-        //}
+        //C# 4.0 寫法  舊方法
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            //AccessUrlForNet4Async();
 
-        //private void AccessUrlForNet4Async()
-        //{
-        //    var client = new WebClient();
-        //    client.DownloadStringCompleted += AccessUrlForNet4AsyncDownloadStringCompleted;
-        //    client.DownloadStringAsync(new Uri("http://msdn.microsoft.com/zh-tw"));
-        //}
+            AccessUrlForNet5Async();
+        }
 
-        //private void AccessUrlForNet4AsyncDownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
-        //{
-        //    GetUrlLength(e.Result);
+        //改用 C# 5.0  async 與 await
+        public async void AccessUrlForNet5Async()
+        {
+            var client = new WebClient();
 
-        //    var client = new WebClient();
+            GetUrlLength(await client.DownloadStringTaskAsync(new Uri("http://msdn.microsoft.com/zh-tw")));
+            GetUrlLength(await client.DownloadStringTaskAsync(new Uri("http://www.microsoft.com/zh-tw")));
+            GetUrlLength(await client.DownloadStringTaskAsync(new Uri("http://channel9.msdn.com/")));
+            GetUrlLength(await client.DownloadStringTaskAsync(new Uri("http://technet.microsoft.com/zh-TW/")));
+        }
 
-        //    client.DownloadStringCompleted += AccessUrlForNet4AsyncDownloadStringCompleted2;
-        //    client.DownloadStringAsync(new Uri("http://www.microsoft.com/zh-tw"));
-        //}
+        private void AccessUrlForNet4Async()
+        {
+            var client = new WebClient();
+            client.DownloadStringCompleted += AccessUrlForNet4AsyncDownloadStringCompleted;
+            client.DownloadStringAsync(new Uri("http://msdn.microsoft.com/zh-tw"));
+        }
 
-        //private void AccessUrlForNet4AsyncDownloadStringCompleted2(object sender, DownloadStringCompletedEventArgs e)
-        //{
-        //    GetUrlLength(e.Result);
+        private void AccessUrlForNet4AsyncDownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            GetUrlLength(e.Result);
 
-        //    var client = new WebClient();
-        //    client.DownloadStringCompleted += AccessUrlForNet4AsyncDownloadStringCompleted3;
-        //    client.DownloadStringAsync(new Uri("http://channe19.msdn.com/"));
-        //}
+            var client = new WebClient();
 
-        //private void AccessUrlForNet4AsyncDownloadStringCompleted3(object sender, DownloadStringCompletedEventArgs e)
-        //{
-        //    GetUrlLength(e.Result);
+            client.DownloadStringCompleted += AccessUrlForNet4AsyncDownloadStringCompleted2;
+            client.DownloadStringAsync(new Uri("http://www.microsoft.com/zh-tw"));
+        }
 
-        //    var client = new WebClient();
-        //    client.DownloadStringCompleted += AccessUrlForNet4AsyncDownloadStringCompleted4;
-        //    client.DownloadStringAsync(new Uri("http://technet.microsoft.com/zh-TW"));
-        //}
+        private void AccessUrlForNet4AsyncDownloadStringCompleted2(object sender, DownloadStringCompletedEventArgs e)
+        {
+            GetUrlLength(e.Result);
 
-        //private void AccessUrlForNet4AsyncDownloadStringCompleted4(object sender, DownloadStringCompletedEventArgs e)
-        //{
-        //    GetUrlLength(e.Result);
+            var client = new WebClient();
+            client.DownloadStringCompleted += AccessUrlForNet4AsyncDownloadStringCompleted3;
+            client.DownloadStringAsync(new Uri("http://channel9.msdn.com/"));
+        }
 
-        //}
+        private void AccessUrlForNet4AsyncDownloadStringCompleted3(object sender, DownloadStringCompletedEventArgs e)
+        {
+            GetUrlLength(e.Result);
 
-        //private void GetUrlLength(string result)
-        //{
-        //    results.Text += "下載 Url 字串長度：" + result.Length + "。\r\n";
-        //}
+            var client = new WebClient();
+            client.DownloadStringCompleted += AccessUrlForNet4AsyncDownloadStringCompleted4;
+            client.DownloadStringAsync(new Uri("http://technet.microsoft.com/zh-TW/"));
+        }
+
+        private void AccessUrlForNet4AsyncDownloadStringCompleted4(object sender, DownloadStringCompletedEventArgs e)
+        {
+            GetUrlLength(e.Result);
+
+        }
+
+        private void GetUrlLength(string result)
+        {
+            results.Text += "下載 Url 字串長度：" + result.Length + "。\r\n";
+        }
+      
     }
 }
